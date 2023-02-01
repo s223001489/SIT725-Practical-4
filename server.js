9412f5195b666547af9546173a73d6d77e7deeb0
s223001489 <s223001489@deakin.edu.au>
2023-02-01 21:30:31 +1100

    update practical 2

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express")
+var app = express()
+
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({extends: false}))
+
+const addNumbers = (number1, number2) => {
+    var num1 = parseInt(number1)
+    var num1 = parseInt(number1)
+    var result = num1+number2;
+    return result;
+
+}
+app.get("/addTwoNumbers", (req,res)=> {
+    var number1 =req.query.number1;
+    var number2 =req.query.number2;
+    var result = addNumbers(number1, number2)
+    req.json({statusCode: 200, data: result, messgae:'Success'})
+})
+
+var port = process.env.port || 3000;
+
+app.listen(port,()=>{
+    console.log("App listening to: https://localhost:"+port)
+})
